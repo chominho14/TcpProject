6195f6cc13f1798eed6193c83bc57a4e91f36031
chominho14 <chominho14@naver.com>
2022-06-11 14:49:24 +0900

    Chating Room CSS

--- client/containers/Messages.tsx
+++ client/containers/Messages.tsx
@@ -1,6 +1,46 @@
 import { useEffect, useRef } from "react";
+import styled from "styled-components";
 import EVENTS from "../config/events";
 import { useSockets } from "../context/socket.context";
+
+// 타이틀
+const ChatingTitle_h2 = styled.h2`
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 20px;
+`;
+
+// 메시지 textarea, button
+const ChatingMessageContainer = styled.div`
+  position: relative;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  margin-left: 10%;
+  margin-right: 10%;
+`;
+
+const ChatingMessaging = styled.div`
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  width: 100%;
+  max-width: 28rem;
+  align-items: center;
+`;
+
+const ChatingMessageText = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: solid 2px gray;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const ChatingMessageButton = styled.button`
+  height: 40px;
+`;
 
 function MessagesContainer() {
   // 방에서 온 소켓 정보들을 받는다
@@ -41,6 +81,8 @@ function MessagesContainer() {
 
   return (
     <div>
+      <hr />
+      <ChatingTitle_h2>채팅하기</ChatingTitle_h2>
       <div>
         {messages.map(({ message, username, time }, index) => {
           return (
@@ -56,14 +98,18 @@ function MessagesContainer() {
         })}
         <div ref={messageEndRef} />
       </div>
-      <div>
-        <textarea
-          rows={1}
-          placeholder="생각하고 있는 것을 말해주세요"
-          ref={newMessageRef}
-        />
-        <button onClick={handleSendMessage}>보내기</button>
-      </div>
+      <ChatingMessageContainer>
+        <ChatingMessaging>
+          <ChatingMessageText
+            rows={1}
+            placeholder="생각하고 있는 것을 말해주세요"
+            ref={newMessageRef}
+          />
+          <ChatingMessageButton onClick={handleSendMessage}>
+            보내기
+          </ChatingMessageButton>
+        </ChatingMessaging>
+      </ChatingMessageContainer>
     </div>
   );
 }
--- client/containers/Rooms.tsx
+++ client/containers/Rooms.tsx
@@ -1,6 +1,83 @@
 import { useRef } from "react";
+import styled from "styled-components";
 import EVENTS from "../config/events";
 import { useSockets } from "../context/socket.context";
+
+const RoomContainer = styled.nav`
+  max-width: 400px;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30px;
+  height: 40vh;
+`;
+
+const RoomCreate = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  padding-bottom: 5px;
+`;
+
+const RoomCreateInput = styled.input`
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 0.375rem;
+  height: 30px;
+`;
+
+const RoomCreateButton = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 0.375rem;
+  text-align: center;
+  background-color: "#808080";
+  color: gray;
+  justify-content: center;
+  margin-top: 5px;
+  height: 30px;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+    color: white;
+  }
+`;
+
+const RoomEntranceTitle_h2 = styled.h2`
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const RoomList_ul = styled.ul`
+  padding-right: 40px;
+`;
+
+const RoomList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+
+const RoomListEnctranceButton = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 0.375rem;
+  text-align: center;
+  background-color: "#808080";
+  color: gray;
+  justify-content: center;
+  margin-top: 5px;
+  height: 30px;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+    color: white;
+  }
+`;
 
 function RoomsContainer() {
   const { socket, roomId, rooms } = useSockets();
@@ -26,30 +103,33 @@ function RoomsContainer() {
   }
 
   return (
-    <nav>
-      <div>
-        <input ref={newRoomRef} placeholder="방 이름" />
-        <button className="cta" onClick={handleCreateRoom}>
+    <RoomContainer>
+      <RoomCreate>
+        <RoomCreateInput ref={newRoomRef} placeholder="방 이름" />
+        <RoomCreateButton className="cta" onClick={handleCreateRoom}>
           방 생성하기
-        </button>
+        </RoomCreateButton>
+      </RoomCreate>
+      <hr />
+      <div>
+        <RoomEntranceTitle_h2>채팅방 들어가기</RoomEntranceTitle_h2>
       </div>
-
-      <ul>
+      <RoomList_ul>
         {Object.keys(rooms).map((key) => {
           return (
-            <div key={key}>
-              <button
+            <RoomList key={key}>
+              <RoomListEnctranceButton
                 disabled={key === roomId}
                 title={`${rooms[key].name} 들어가기`}
                 onClick={() => handleJoinRoom(key)}
               >
-                {rooms[key].name}
-              </button>
-            </div>
+                {rooms[key].name}에 입장하기
+              </RoomListEnctranceButton>
+            </RoomList>
           );
         })}
-      </ul>
-    </nav>
+      </RoomList_ul>
+    </RoomContainer>
   );
 }
 
